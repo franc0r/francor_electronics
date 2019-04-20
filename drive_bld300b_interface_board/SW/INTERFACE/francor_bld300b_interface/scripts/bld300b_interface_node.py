88de690d5ce73812980f6968c77fe413c24e5834
Martin Bauernschmitt <martin.bauernschmitt@posteo.de>
2019-04-20 16:18:46 +0200

    Added timeout for every drive

--- drive_bld300b_interface_board/SW/INTERFACE/francor_bld300b_interface/scripts/bld300b_interface_node.py
+++ drive_bld300b_interface_board/SW/INTERFACE/francor_bld300b_interface/scripts/bld300b_interface_node.py
@@ -1,39 +1,59 @@
 #!/usr/bin/env python
 import rospy
+import sys
+import serial
 from std_msgs.msg import Float32
 
-g_rx_timestamp = rospy.Time()
+g_rx_timestamp_m1 = rospy.Time()
+g_rx_timestamp_m2 = rospy.Time()
+g_rx_timestamp_m3 = rospy.Time()
+g_rx_timestamp_m4 = rospy.Time()
+
 g_duty_cycle_m1 = 0.0
 g_duty_cycle_m2 = 0.0
 g_duty_cycle_m3 = 0.0
 g_duty_cycle_m4 = 0.0
 
-max_timeout = 5.0
+max_timeout = 0.1
 
 def callback_M1(data):
-  global g_rx_timestamp, g_duty_cycle_m1
+  global g_rx_timestamp_m1, g_duty_cycle_m1
   g_duty_cycle_m1 = float(data.data)
-  g_rx_timestamp = rospy.Time.now()
+  g_rx_timestamp_m1 = rospy.Time.now()
 
 def callback_M2(data):
-  global g_rx_timestamp, g_duty_cycle_m2
+  global g_rx_timestamp_m2, g_duty_cycle_m2
   g_duty_cycle_m2 = float(data.data)
-  g_rx_timestamp = rospy.Time.now()
+  g_rx_timestamp_m2 = rospy.Time.now()
 
 def callback_M3(data):
-  global g_rx_timestamp, g_duty_cycle_m3
+  global g_rx_timestamp_m3, g_duty_cycle_m3
   g_duty_cycle_m3 = float(data.data)
-  g_rx_timestamp = rospy.Time.now()
+  g_rx_timestamp_m3 = rospy.Time.now()
 
 def callback_M4(data):
-  global g_rx_timestamp, g_duty_cycle_m4
+  global g_rx_timestamp_m4, g_duty_cycle_m4
   g_duty_cycle_m4 = float(data.data)
-  g_rx_timestamp = rospy.Time.now()
+  g_rx_timestamp_m4 = rospy.Time.now()
 
 
 if __name__ == '__main__':
   # Init ROS
   rospy.init_node('drive_300b_interface')
+
+  # Init variables
+  g_rx_timestamp_m1 = rospy.Time.now()
+  g_rx_timestamp_m2 = rospy.Time.now()
+  g_rx_timestamp_m3 = rospy.Time.now()
+  g_rx_timestamp_m4 = rospy.Time.now()
+
+
+  # Open serial device
+  try:
+    nucleo_serial = serial.Serial('/dev/ttyS8', 115200)
+  except Exception as e:
+    rospy.logerr("Failed to open serial device!");
+    sys.exit(-1)
 
   # Register subscribers
   rospy.Subscriber('/drive/m1/speed', Float32, callback_M1)
@@ -42,22 +62,47 @@ if __name__ == '__main__':
   rospy.Subscriber('/drive/m4/speed', Float32, callback_M4)
 
   # Run loop
-  rate = rospy.Rate(1)
+  rate = rospy.Rate(20)
   while not rospy.is_shutdown():
-    # Calculate last message time
-    delta_rx_time = rospy.Time.now() - g_rx_timestamp
-    if(delta_rx_time.to_sec() < max_timeout):
-      # Convert to fixed point
-      duty_cycle_m1 = int(g_duty_cycle_m1 * 100.0)
-      duty_cycle_m2 = int(g_duty_cycle_m2 * 100.0)
-      duty_cycle_m3 = int(g_duty_cycle_m3 * 100.0)
-      duty_cycle_m4 = int(g_duty_cycle_m4 * 100.0)
+    # Calculate last message timestamps
+    delta_rx_time_m1 = rospy.Time.now() - g_rx_timestamp_m1
+    delta_rx_time_m2 = rospy.Time.now() - g_rx_timestamp_m2
+    delta_rx_time_m3 = rospy.Time.now() - g_rx_timestamp_m3
+    delta_rx_time_m4 = rospy.Time.now() - g_rx_timestamp_m4
 
-      # Generate serial string
-      data = ("SPD %+.5i %+.5i %+.5i %+.5i\r\n" % (duty_cycle_m1, duty_cycle_m2, duty_cycle_m3, duty_cycle_m4))
+    # Check if last message for every individual drive is to long ago
+    # set speed to 0 after timeout
+    if(delta_rx_time_m1.to_sec() > max_timeout):
+      g_duty_cycle_m1 = 0.0
+      rospy.logwarn("Timeout M1")     
 
-    else:
-      print("Timeout last message " + str(delta_rx_time.to_sec()) + " seconds ago")
+    if(delta_rx_time_m2.to_sec() > max_timeout):
+      g_duty_cycle_m2 = 0.0
+      rospy.logwarn("Timeout M2")  
+
+    if(delta_rx_time_m3.to_sec() > max_timeout):
+      g_duty_cycle_m3 = 0.0
+      rospy.logwarn("Timeout M3")  
+
+    if(delta_rx_time_m4.to_sec() > max_timeout):
+      g_duty_cycle_m4 = 0.0
+      rospy.logwarn("Timeout M4")  
+
+    # Convert to fixed point
+    duty_cycle_m1 = int(g_duty_cycle_m1 * 100.0)
+    duty_cycle_m2 = int(g_duty_cycle_m2 * 100.0)
+    duty_cycle_m3 = int(g_duty_cycle_m3 * 100.0)
+    duty_cycle_m4 = int(g_duty_cycle_m4 * 100.0)
+
+    # Generate serial string
+    data = ("SPD %+.5i %+.5i %+.5i %+.5i\r\n" % (duty_cycle_m1, duty_cycle_m2, duty_cycle_m3, duty_cycle_m4))
+    
+    # Send data
+    try:
+      nucleo_serial.write(data)
+    except Exception as ed:
+      rospy.logerr("Failed to send data to nucleo board!")
+      sys.exit(-1)
     
     rate.sleep()
 
